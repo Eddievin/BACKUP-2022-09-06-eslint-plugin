163309d3ddfaa883af60d10be07b51e1775b37fd
Ilya Lyubinskiy <admin.phpdev@gmail.com>
2022-02-25 21:15:30 +0300

    chore(eslint): Edited configuration

--- .eslintrc.js
+++ .eslintrc.js
@@ -1,22 +1,3 @@
-const config = require("@skylib/config/src/eslintrc");
-
-const consistentImport = config.rules["@skylib/consistent-import"];
-
 module.exports = {
-  extends: [require.resolve("@skylib/config/src/eslintrc")],
-  rules: {
-    "@skylib/consistent-import": [
-      consistentImport[0],
-      {
-        ...consistentImport[1],
-        sources: [
-          ...consistentImport[1].sources,
-          {
-            sourcePattern: "@skylib/eslint-plugin/src/rules/utils",
-            type: "wildcard"
-          }
-        ]
-      }
-    ]
-  }
+  extends: [require.resolve("@skylib/config/src/eslintrc")]
 };
