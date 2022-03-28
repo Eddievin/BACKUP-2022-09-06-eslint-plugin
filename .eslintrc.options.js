f88e7b5409f98e33d3b85da48c9531007df99633
Ilya Lyubinskiy <admin.phpdev@gmail.com>
2022-03-29 02:55:30 +0300

    chore(fix): After "@skylib/config" update

--- .eslintrc.options.js
+++ .eslintrc.options.js
@@ -1,4 +1,22 @@
 module.exports = {
-  readonlyIgnoreTypes: [],
+  consistentImport: [
+    { sourcePattern: "estree", type: "wildcard" },
+    {
+      localName: "ts",
+      sourcePattern: "typescript",
+      type: "wildcard"
+    },
+    { sourcePattern: "tsutils", type: "wildcard" },
+    {
+      autoImportSource: "@/rules/utils",
+      sourcePattern: "@skylib/eslint-plugin/src/rules/utils",
+      type: "wildcard"
+    }
+  ],
+  extends: [
+    require("@skylib/functions/src/configs/eslintrc.options")(
+      "@skylib/functions/dist/"
+    )
+  ],
   utility: true
 };
