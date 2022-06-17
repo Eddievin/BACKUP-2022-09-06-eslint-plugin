4d91a5cb93f64c0ab77ef6ae96fdf74c34b464b8
Ilya Lyubinskiy <admin.phpdev@gmail.com>
2022-06-17 23:15:56 +0300

    feat(rules): Added rule copies; Added configs

--- src/rules/index.ts
+++ src/rules/index.ts
@@ -38,48 +38,74 @@ import { statementsOrder } from "./statements-order";
 import { switchCaseEmptyLines } from "./switch-case-empty-lines";
 import { templateLiteralFormat } from "./template-literal-format";
 import { vueComponentName } from "./vue-component-name";
+import { evaluate, o } from "@skylib/functions";
+import type { WritableIndexedObject } from "@skylib/functions";
 
 export * as utils from "./utils";
 
-export const rules = {
-  "array-callback-return-type": arrayCallbackReturnType,
-  "class-member-typedef": classMemberTypedef,
-  "class-name": className,
-  "class-only-export": classOnlyExport,
-  "consistent-empty-lines": consistentEmptyLines,
-  "consistent-filename": consistentFilename,
-  "consistent-group-empty-lines": consistentGroupEmptyLines,
-  "consistent-import": consistentImport,
-  "disallow-by-regexp": disallowByRegexp,
-  "disallow-identifier": disallowIdentifier,
-  "disallow-import": disallowImport,
-  "empty-lines-around-comment": emptyLinesAroundComment,
-  "exhaustive-switch": exhaustiveSwitch,
-  "export-all-name": exportAllName,
-  "function-properties-style": functionPropertiesStyle,
-  "no-expression-empty-line": noExpressionEmptyLine,
-  "no-inferrable-types": noInferrableTypes,
-  "no-multi-type-tuples": noMultiTypeTuples,
-  "no-mutable-signature": noMutableSignature,
-  "no-negated-condition": noNegatedCondition,
-  "no-restricted-syntax": noRestrictedSyntax,
-  "no-unnecessary-readonly": noUnnecessaryReadonly,
-  "no-unnecessary-writable": noUnnecessaryWritable,
-  "no-unsafe-object-assignment": noUnsafeObjectAssignment,
-  "no-unused-import": noUnusedImport,
-  "object-format": objectFormat,
-  "only-export-name": onlyExportName,
-  "optional-property-style": optionalPropertyStyle,
-  "prefer-alias-for-array-types": preferAliasForArrayTypes,
-  "prefer-readonly": preferReadonly,
-  "prefer-readonly-props": preferReadonlyProps,
-  "prefer-ts-toolbelt": preferTsToolbelt,
-  "primary-export-only": primaryExportOnly,
-  "require-jsdoc": requireJsdoc,
-  "sort-class-members": sortClassMembers,
-  "sort-keys": sortKeys,
-  "statements-order": statementsOrder,
-  "switch-case-empty-lines": switchCaseEmptyLines,
-  "template-literal-format": templateLiteralFormat,
-  "vue-component-name": vueComponentName
-};
+export const { configs, rules } = evaluate(() => {
+  const core = {
+    "array-callback-return-type": arrayCallbackReturnType,
+    "class-member-typedef": classMemberTypedef,
+    "class-name": className,
+    "class-only-export": classOnlyExport,
+    "consistent-empty-lines": consistentEmptyLines,
+    "consistent-filename": consistentFilename,
+    "consistent-group-empty-lines": consistentGroupEmptyLines,
+    "consistent-import": consistentImport,
+    "disallow-by-regexp": disallowByRegexp,
+    "disallow-identifier": disallowIdentifier,
+    "disallow-import": disallowImport,
+    "empty-lines-around-comment": emptyLinesAroundComment,
+    "exhaustive-switch": exhaustiveSwitch,
+    "export-all-name": exportAllName,
+    "function-properties-style": functionPropertiesStyle,
+    "no-expression-empty-line": noExpressionEmptyLine,
+    "no-inferrable-types": noInferrableTypes,
+    "no-multi-type-tuples": noMultiTypeTuples,
+    "no-mutable-signature": noMutableSignature,
+    "no-negated-condition": noNegatedCondition,
+    "no-restricted-syntax": noRestrictedSyntax,
+    "no-unnecessary-readonly": noUnnecessaryReadonly,
+    "no-unnecessary-writable": noUnnecessaryWritable,
+    "no-unsafe-object-assignment": noUnsafeObjectAssignment,
+    "no-unused-import": noUnusedImport,
+    "object-format": objectFormat,
+    "only-export-name": onlyExportName,
+    "optional-property-style": optionalPropertyStyle,
+    "prefer-alias-for-array-types": preferAliasForArrayTypes,
+    "prefer-readonly": preferReadonly,
+    "prefer-readonly-props": preferReadonlyProps,
+    "prefer-ts-toolbelt": preferTsToolbelt,
+    "primary-export-only": primaryExportOnly,
+    "require-jsdoc": requireJsdoc,
+    "sort-class-members": sortClassMembers,
+    "sort-keys": sortKeys,
+    "statements-order": statementsOrder,
+    "switch-case-empty-lines": switchCaseEmptyLines,
+    "template-literal-format": templateLiteralFormat,
+    "vue-component-name": vueComponentName
+  };
+
+  const copies: WritableIndexedObject = {};
+
+  for (const [name, rule] of o.entries(core)) {
+    copies[`facades/${name}`] = rule;
+    copies[`framework/${name}`] = rule;
+    copies[`functions/${name}`] = rule;
+    copies[`project/${name}`] = rule;
+    copies[`quasar-extension/${name}`] = rule;
+  }
+
+  return {
+    configs: {
+      all: {
+        plugins: ["@skylib/eslint-plugin"],
+        rules: o.fromEntries(
+          o.keys(core).map(name => [`@skylib/${name}`, "warn"])
+        )
+      }
+    },
+    rules: { ...core, ...copies }
+  };
+});
