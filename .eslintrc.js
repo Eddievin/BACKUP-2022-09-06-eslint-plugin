3b39c80c1f0362144932e75eb299520aab0727cd
Ilia Liubinskii <admin.phpdev@gmail.com>
2022-06-24 11:55:35 +0300

    chore(eslint): Added "@skylib/functions" rules

--- .eslintrc.js
+++ .eslintrc.js
@@ -1,6 +1,7 @@
 module.exports = {
   extends: [
     require.resolve("@skylib/config/src/eslintrc"),
+    require.resolve("@skylib/functions/configs/eslintrc"),
     "./.eslintrc.overrides",
     "./.eslintrc.temp"
   ]
