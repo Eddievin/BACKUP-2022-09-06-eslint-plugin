6b1adf6dc873cb4d2fcb4bdf7398f91a02e7de1f
Ilia Liubinskii <admin.phpdev@gmail.com>
2022-07-09 19:40:50 +0300

    chore(eslint): Edited rules

--- .eslintrc.overrides.js
+++ .eslintrc.overrides.js
@@ -1,12 +1,7 @@
 module.exports = {
-  extends: require.resolve("@skylib/config/src/eslintrc.allow-nodejs-modules"),
-  overrides: [
-    {
-      files: "src/rules/utils/synonyms.ts",
-      extends: [
-        require.resolve("@skylib/config/src/eslintrc.allow-require"),
-        require.resolve("@skylib/config/src/eslintrc.allow-require-unsafe")
-      ]
-    }
+  extends: [
+    require.resolve("@skylib/config/src/eslintrc.allow-nodejs-modules"),
+    require.resolve("@skylib/config/src/eslintrc.allow-require"),
+    require.resolve("@skylib/config/src/eslintrc.allow-require-unsafe")
   ]
 };
--- .eslintrc.rule-overrides.js
+++ .eslintrc.rule-overrides.js
@@ -1,1 +1,42 @@
-module.exports = {};
+module.exports = {
+  rules: {
+    "@skylib/sort-keys": [
+      "warn",
+      {
+        overrides: [
+          {
+            _id: "utils-createRule",
+            customOrder: [
+              "name",
+              "fixable",
+              "isOptions",
+              "defaultOptions",
+              "subOptionsKey",
+              "isSubOptions",
+              "defaultSubOptions",
+              "messages",
+              "create"
+            ],
+            selector:
+              "CallExpression[callee.object.name=utils][callee.property.name=createRule] > ObjectExpression"
+          },
+          {
+            _id: "utils-testRule",
+            customOrder: [
+              "only",
+              "name",
+              "filename",
+              "settings",
+              "options",
+              "code",
+              "output",
+              "errors"
+            ],
+            selector:
+              "CallExpression[callee.object.name=utils][callee.property.name=testRule] > ArrayExpression > ObjectExpression"
+          }
+        ]
+      }
+    ]
+  }
+};
