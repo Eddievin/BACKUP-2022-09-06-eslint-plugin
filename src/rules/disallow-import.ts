4ffa50f2e957713023bd10327b38a783cadd80d1
Ilia Liubinskii <admin.phpdev@gmail.com>
2022-07-09 10:54:30 +0300

    feat(disallow-import): Check require and re-export

--- src/rules/disallow-import.ts
+++ src/rules/disallow-import.ts
@@ -1,7 +1,8 @@
 import * as utils from "./utils";
-import { as, is } from "@skylib/functions";
 import { AST_NODE_TYPES } from "@typescript-eslint/utils";
 import type { RuleListener } from "@typescript-eslint/utils/dist/ts-eslint";
+import type { TSESTree } from "@typescript-eslint/utils";
+import { is } from "@skylib/functions";
 import type { strings } from "@skylib/functions";
 
 export const disallowImport = utils.createRule({
@@ -14,13 +15,48 @@ export const disallowImport = utils.createRule({
     );
 
     return {
-      [AST_NODE_TYPES.ImportDeclaration]: (node): void => {
-        const source = as.string(node.source.value);
+      [AST_NODE_TYPES.CallExpression]: (node): void => {
+        if (
+          node.callee.type === AST_NODE_TYPES.Identifier &&
+          node.callee.name === "require"
+        ) {
+          const source = node.arguments[0];
 
-        if (matcher(source))
-          context.report({ messageId: "disallowedSource", node });
+          if (
+            is.not.empty(source) &&
+            source.type === AST_NODE_TYPES.Literal &&
+            is.string(source.value)
+          )
+            lintNode(source.value, source);
+        }
+      },
+      [AST_NODE_TYPES.ImportDeclaration]: (node): void => {
+        const source = node.source;
+
+        lintNode(source.value, source);
+      },
+      [AST_NODE_TYPES.ExportAllDeclaration]: (node): void => {
+        const source = node.source;
+
+        lintNode(source.value, source);
+      },
+      [AST_NODE_TYPES.ExportNamedDeclaration]: (node): void => {
+        const source = node.source;
+
+        if (source) lintNode(source.value, source);
+      },
+      [AST_NODE_TYPES.ImportExpression]: (node): void => {
+        const source = node.source;
+
+        if (source.type === AST_NODE_TYPES.Literal && is.string(source.value))
+          lintNode(source.value, source);
       }
     };
+
+    function lintNode(source: string, node: TSESTree.Node): void {
+      if (matcher(source))
+        context.report({ messageId: "disallowedSource", node });
+    }
   },
   defaultOptions: { allow: [], disallow: [] },
   isRuleOptions: is.object.factory<RuleOptions>(
--- tests/disallow-import.ts
+++ tests/disallow-import.ts
@@ -39,6 +39,67 @@ utils.testRule("disallow-import", rules, [
       import "@/source";
       import "./source";
       import "../source";
+      import "source";
+    `,
+    errors: [
+      { line: 1, messageId: "disallowedSource" },
+      { line: 2, messageId: "disallowedSource" },
+      { line: 3, messageId: "disallowedSource" }
+    ],
+    name: `Test at line ${getCurrentLine().line}`,
+    options: [{ disallow: ["@/*", "./*", "../*"] }]
+  },
+  {
+    code: `
+      const x1 = import("@/source");
+      const x2 = import("./source");
+      const x3 = import("../source");
+      const x4 = import("source");
+    `,
+    errors: [
+      { line: 1, messageId: "disallowedSource" },
+      { line: 2, messageId: "disallowedSource" },
+      { line: 3, messageId: "disallowedSource" }
+    ],
+    name: `Test at line ${getCurrentLine().line}`,
+    options: [{ disallow: ["@/*", "./*", "../*"] }]
+  },
+  {
+    code: `
+      const x1 = require("@/source");
+      const x2 = require("./source");
+      const x3 = require("../source");
+      const x4 = require("source");
+    `,
+    errors: [
+      { line: 1, messageId: "disallowedSource" },
+      { line: 2, messageId: "disallowedSource" },
+      { line: 3, messageId: "disallowedSource" }
+    ],
+    name: `Test at line ${getCurrentLine().line}`,
+    options: [{ disallow: ["@/*", "./*", "../*"] }]
+  },
+  {
+    code: `
+      export * from "@/source";
+      export * from "./source";
+      export * from "../source";
+      export * from "source";
+    `,
+    errors: [
+      { line: 1, messageId: "disallowedSource" },
+      { line: 2, messageId: "disallowedSource" },
+      { line: 3, messageId: "disallowedSource" }
+    ],
+    name: `Test at line ${getCurrentLine().line}`,
+    options: [{ disallow: ["@/*", "./*", "../*"] }]
+  },
+  {
+    code: `
+      export { x1 } from "@/source";
+      export { x2 } from "./source";
+      export { x3 } from "../source";
+      export { x4 } from "source";
     `,
     errors: [
       { line: 1, messageId: "disallowedSource" },
