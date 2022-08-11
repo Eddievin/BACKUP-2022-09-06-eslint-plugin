f80dcb59ae25aac0b7724a65ac0bdb5a937b678c
Ilia Liubinskii <admin.phpdev@gmail.com>
2022-08-11 23:38:13 +0300

    feat(prefer-enum): Edited rule

--- src/typescript/prefer-enum.ts
+++ src/typescript/prefer-enum.ts
@@ -4,8 +4,11 @@ import { core } from "./core";
 export const preferEnum = utils.wrapRule(core["no-restricted-syntax"], [
   {
     message: "Use enum instead",
-    selector:
-      "TSTypeAliasDeclaration > TSUnionType > TSLiteralType:first-child",
+    selector: [
+      "TSTypeAliasDeclaration[typeAnnotation.types.0.literal]",
+      "TSTypeAliasDeclaration[typeAnnotation.types.1.literal]",
+      "TSTypeAliasDeclaration[typeAnnotation.types.2.literal]"
+    ],
     typeIs: utils.TypeGroup.string
   }
 ]);
--- tests/typescript/prefer-enum.ts
+++ tests/typescript/prefer-enum.ts
@@ -8,7 +8,11 @@ const MessageId = utils.getMessageId(rule);
 utils.testRule("prefer-enum", rule, [
   {
     name: `Test at line ${getCurrentLine().line}`,
-    code: 'type T = "a" | "b";',
-    errors: [{ line: 1, messageId: MessageId.customMessage }]
+    code: `
+      type T =
+        | "a"
+        | "b";
+    `,
+    errors: [{ line: 1, endLine: 3, messageId: MessageId.customMessage }]
   }
 ]);
