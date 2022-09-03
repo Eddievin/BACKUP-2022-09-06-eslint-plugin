0e8f0212ad7a61741f26a24023526f17a65f33ff
Ilia Liubinskii <admin.phpdev@gmail.com>
2022-09-03 17:19:46 +0300

    feat(consistent-suboptions-id): Added rule

--- src/eslintrc/index.ts
+++ src/eslintrc/index.ts
@@ -1,3 +1,4 @@
+import { consistentSuboptionsId } from "./consistent-suboptions-id";
 import { noUnnecessaryArray } from "./no-unnecessary-array";
 import { o } from "@skylib/functions";
 import { sortArray } from "./sort-array";
@@ -5,6 +6,7 @@ import { sortSuboptions } from "./sort-suboptions";
 
 export const eslintrc = o.prefixKeys(
   {
+    "consistent-suboptions-id": consistentSuboptionsId,
     "no-unnecessary-array": noUnnecessaryArray,
     "sort-array": sortArray,
     "sort-suboptions": sortSuboptions
