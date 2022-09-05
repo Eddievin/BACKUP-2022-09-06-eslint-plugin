09ec482b4be70731c91e0368e1f294631ab49af2
Ilia Liubinskii <admin.phpdev@gmail.com>
2022-09-05 23:34:51 +0300

    feat(configs): Edited configurations

--- src/configs.ts
+++ src/configs.ts
@@ -1,8 +1,8 @@
+import { evaluate, o } from "@skylib/functions";
 import type { IndexedRecord } from "@skylib/functions";
 import { eslintrc } from "./eslintrc";
 import { jest } from "./jest";
 import { misc } from "./misc";
-import { o } from "@skylib/functions";
 import { skylibConfig } from "./skylib-config";
 import { skylibFacades } from "./skylib-facades";
 import { skylibFramework } from "./skylib-framework";
@@ -11,91 +11,77 @@ import { skylibQuasarExtension } from "./skylib-quasar-extension";
 import { typescript } from "./typescript";
 import { vue } from "./vue";
 
-export const configs = {
-  "all": {
-    plugins: ["@skylib/eslint-plugin"],
-    rules: {
-      ...rules(misc),
-      "@skylib/match-filename": "off",
-      "@skylib/no-restricted-syntax": "off",
-      "@skylib/require-syntax": "off",
-      "@skylib/wrap": "off"
+export const configs = evaluate((): IndexedRecord => {
+  const result = {
+    "eslintrc": { rules: rules(eslintrc) },
+    "jest": { rules: rules(jest) },
+    "misc": {
+      rules: {
+        ...rules(misc),
+        "@skylib/match-filename": "off",
+        "@skylib/no-restricted-syntax": "off",
+        "@skylib/require-syntax": "off",
+        "@skylib/wrap": "off"
+      }
+    },
+    "skylib-config": { rules: rules(skylibConfig) },
+    "skylib-facades": { rules: rules(skylibFacades) },
+    "skylib-framework": { rules: rules(skylibFramework) },
+    "skylib-functions.jest": { rules: rules(skylibFunctions.jest) },
+    "skylib-functions.misc": { rules: rules(skylibFunctions.misc) },
+    "skylib-quasar-extension.extras": {
+      rules: {
+        ...rules(skylibQuasarExtension.extras),
+        "@skylib/typescript/no-empty-interfaces": "off"
+      }
+    },
+    "skylib-quasar-extension.jest": {
+      rules: rules(skylibQuasarExtension.jest)
+    },
+    "skylib-quasar-extension.misc": {
+      rules: rules(skylibQuasarExtension.misc)
+    },
+    "skylib-quasar-extension.vue": { rules: rules(skylibQuasarExtension.vue) },
+    "typescript": {
+      rules: {
+        ...rules(typescript),
+        "@skylib/typescript/no-restricted-syntax": "off"
+      }
+    },
+    "vue": {
+      rules: {
+        ...rules(vue),
+        "@skylib/typescript/no-complex-declarator-type": "off",
+        "@skylib/typescript/no-complex-return-type": "off"
+      }
     }
-  },
-  "config": { plugins: ["@skylib/eslint-plugin"], rules: rules(skylibConfig) },
-  "eslintrc": { plugins: ["@skylib/eslint-plugin"], rules: rules(eslintrc) },
-  "facades": {
-    plugins: ["@skylib/eslint-plugin"],
-    rules: rules(skylibFacades)
-  },
-  "framework": {
-    plugins: ["@skylib/eslint-plugin"],
-    rules: rules(skylibFramework)
-  },
-  "functions": {
-    overrides: [{ files: "./tests/**", rules: rules(skylibFunctions.jest) }],
-    plugins: ["@skylib/eslint-plugin"],
-    rules: rules(skylibFunctions.misc)
-  },
-  "functions.jest": {
-    plugins: ["@skylib/eslint-plugin"],
-    rules: rules(skylibFunctions.jest)
-  },
-  "functions.misc": {
-    plugins: ["@skylib/eslint-plugin"],
-    rules: rules(skylibFunctions.misc)
-  },
-  "jest": { plugins: ["@skylib/eslint-plugin"], rules: rules(jest) },
-  "quasar-extension": {
-    overrides: [
-      {
-        files: "*.extras",
-        rules: {
-          ...rules(skylibQuasarExtension.extras),
-          "@skylib/typescript/no-empty-interfaces": "off"
-        }
-      },
-      { files: "*.vue", rules: rules(skylibQuasarExtension.vue) },
-      { files: "./tests/**", rules: rules(skylibQuasarExtension.jest) }
-    ],
-    plugins: ["@skylib/eslint-plugin"],
-    rules: rules(skylibQuasarExtension.misc)
-  },
-  "quasar-extension.extras": {
-    plugins: ["@skylib/eslint-plugin"],
-    rules: {
-      ...rules(skylibQuasarExtension.extras),
-      "@skylib/typescript/no-empty-interfaces": "off"
+  } as const;
+
+  return {
+    ...result,
+    "all": {
+      ...result.misc,
+      overrides: [
+        { files: "!*.js", ...result.typescript },
+        { files: "*.vue", ...result.vue },
+        { files: "./tests/**", ...result.jest },
+        { files: ".eslintrc.js", ...result.eslintrc }
+      ]
+    },
+    "skylib-functions": {
+      ...result["skylib-functions.misc"],
+      overrides: [{ files: "./tests/**", ...result["skylib-functions.jest"] }]
+    },
+    "skylib-quasar-extension": {
+      ...result["skylib-quasar-extension.misc"],
+      overrides: [
+        { files: "*.extras", ...result["skylib-quasar-extension.extras"] },
+        { files: "*.vue", ...result["skylib-quasar-extension.vue"] },
+        { files: "./tests/**", ...result["skylib-quasar-extension.jest"] }
+      ]
     }
-  },
-  "quasar-extension.jest": {
-    plugins: ["@skylib/eslint-plugin"],
-    rules: rules(skylibQuasarExtension.jest)
-  },
-  "quasar-extension.misc": {
-    plugins: ["@skylib/eslint-plugin"],
-    rules: rules(skylibQuasarExtension.misc)
-  },
-  "quasar-extension.vue": {
-    plugins: ["@skylib/eslint-plugin"],
-    rules: rules(skylibQuasarExtension.vue)
-  },
-  "typescript": {
-    plugins: ["@skylib/eslint-plugin"],
-    rules: {
-      ...rules(typescript),
-      "@skylib/typescript/no-restricted-syntax": "off"
-    }
-  },
-  "vue": {
-    plugins: ["@skylib/eslint-plugin"],
-    rules: {
-      ...rules(vue),
-      "@skylib/typescript/no-complex-declarator-type": "off",
-      "@skylib/typescript/no-complex-return-type": "off"
-    }
-  }
-} as const;
+  };
+});
 
 /**
  * Converts rules to configuration.
